b662532f39162fd7b2cdf96a26b427d595c0d500
AnkitNihalchandani <ankitnehalchandani60@gmail.com>
2023-03-14 17:55:26 +0530

    Done 1

---
+++ app/src/main/java/com/example/traveller/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.traveller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button4login;
+    Button button4registration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
